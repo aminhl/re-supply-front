c46e8fe99dee357a9777a13ee2cf43ea9dc3ff33
aminhl <amin.hlel@esprit.tn>
2023-04-17 05:52:46 +0100

    update_password_refactor

--- src/app/client/components/update-password/update-password.component.ts
+++ src/app/client/components/update-password/update-password.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { AuthService } from "../../../shared/services/auth.service";
+import Swal from "sweetalert2";
 
 @Component({
   selector: 'app-update-password',
@@ -43,12 +44,27 @@ export class UpdatePasswordComponent implements OnInit {
   }
 
   errorMessage!: string;
+  successMessage!: string;
 
   onSubmit() {
     if (this.password.dirty && this.confirmPassword.dirty){
       if(this.confirmPassword.value === this.password.value)
         this.authService.updatePassword("users/updatePassword", this.updatePasswordForm.value)
-          .subscribe((response: any) => console.log("data"), (err) => {
+          .subscribe((response: any) => {
+            this.successMessage = "Everything is OK!";
+            this.errorMessage = "";
+            setTimeout(() => {
+              Swal.fire({
+                title: 'Password changed',
+                text: 'Your password has been changed successfully',
+                icon: 'success'
+              });
+              localStorage.setItem('jwt', response.token);
+              this.passwordCurrent.patchValue("");
+              this.password.patchValue("");
+              this.confirmPassword.patchValue("");
+            },2000)
+          }, (err) => {
             if (err.error.message === "Your current password is wrong.")
               this.errorMessage = "Current Password is wrong!";
           });
