22b36b10fc02f4ee792620ae5c0cc3455037eb4c
NaderAyed <nader.ayed@esprit.tn>
2023-04-18 17:58:18 +0100

    Management dashboard

--- src/app/admin/components/dashboard/user-management/user-management.component.ts
+++ src/app/admin/components/dashboard/user-management/user-management.component.ts
@@ -1,21 +1,18 @@
-
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
-import { AuthService } from '../../../shared/services/auth.service';
-import { HttpClient } from '@angular/common/http';
-import { environment as env } from '../../../../environments/environment';
-import { type } from 'os';
-import { catchError, EMPTY, tap } from 'rxjs';
-import { AdminService } from "../../services/admin.service";
-import Swal from 'sweetalert2';
-import {ProductService} from "../../../shared/services/product.service";
-import {RequestService} from "../../../shared/services/request.service";
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {AuthService} from "../../../../shared/services/auth.service";
+import {AdminService} from "../../../services/admin.service";
+import {ProductService} from "../../../../shared/services/product.service";
+import {RequestService} from "../../../../shared/services/request.service";
+import {HttpClient} from "@angular/common/http";
+import Swal from "sweetalert2";
+import {environment as env} from "../../../../../environments/environment";
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css'],
+  selector: 'app-user-management',
+  templateUrl: './user-management.component.html',
+  styleUrls: ['./user-management.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class UserManagementComponent implements OnInit {
   role: String;
   sevenDaysUsers: any;
   originalUsers: any;
@@ -27,8 +24,8 @@ export class DashboardComponent implements OnInit {
   products: any;
   order: string = 'status';
   reverse: boolean = false;
-   donations: any;
-   selectedComponent:string="user";
+  donations: any;
+  selectedComponent:string="user";
 
   constructor(
     private authService: AuthService,
@@ -41,7 +38,6 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit(): void {
     this.getAllUsers();
-    this.getAllProducts();
 
   }
 
@@ -177,65 +173,8 @@ export class DashboardComponent implements OnInit {
     });
   }
 
-  deleteProduct(productId: string) {
-    Swal.fire({
-      title: 'Are you sure you want to delete this product?',
-      text: 'This action cannot be undone',
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonText: 'Yes, delete it!',
-      cancelButtonText: 'Cancel',
-    }).then((result) => {
-      if (result.isConfirmed) {
-        this.http
-          .delete(`${env.apiRoot}products/${productId}`)
-          .subscribe(() => {
-            this.products = this.products.filter(
-              (product) => product._id !== productId
-            );
-            Swal.fire('Product deleted', '', 'success');
-          });
-      }
-    });
-  }
 
-  acceptProduct(productId: string) {
-    const product = this.products.find((p: any) => p._id === productId);
-    if (product.status === 'accepted') {
-      Swal.fire('Product already accepted', '', 'warning');
-      return;
-    }
-    Swal.fire({
-      title: 'Are you sure you want to accept this product?',
-      text: 'This action cannot be undone',
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonText: 'Yes, accept it!',
-      cancelButtonText: 'Cancel',
-    }).then((result) => {
-      if (result.isConfirmed) {
-        this.http
-          .patch(`${env.apiRoot}products/accept/${productId}`, {})
-          .subscribe(() => {
-            this.getAllProducts();
-            Swal.fire('Product accepted', '', 'success');
-          });
-      }
-    });
-  }
 
-  getAllProducts() {
-    return this.productService.getAllProducts().subscribe((response: any) => {
-      this.products = response.data.products.map((product: any) => {
-        if (product.status === 'pending') {
-          product.color = '#ffe6e6';
-        } else if (product.status === 'accepted') {
-          product.color = '#e6ffed';
-        }
-        return product;
-      });
-    });
-  }
   sort(column: string) {
     if (this.order === column) {
       this.reverse = !this.reverse;
@@ -250,6 +189,5 @@ export class DashboardComponent implements OnInit {
     }
     return user.phoneNumber;
   }
+
 }
-
-
