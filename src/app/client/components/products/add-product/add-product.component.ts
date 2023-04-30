6e759704388196721e5a4c5af9a824c25a02553e
KhairouneAymen <aymen.khairoune@esprit.tn>
2023-04-30 15:54:24 +0100

    fixing cart/claims dashboard

--- src/app/client/components/products/add-product/add-product.component.ts
+++ src/app/client/components/products/add-product/add-product.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import {ProductService} from "../../../../shared/services/product.service";
-import {Router} from "@angular/router";
-import {regexValid} from "../../../../core/validators/signup.validator";
-import { AuthService } from "../../../../shared/services/auth.service";
+import { ProductService } from '../../../../shared/services/product.service';
+import { Router } from '@angular/router';
+import { regexValid } from '../../../../core/validators/signup.validator';
+import { AuthService } from '../../../../shared/services/auth.service';
 
 @Component({
   selector: 'app-add-product',
   templateUrl: './add-product.component.html',
-  styleUrls: ['./add-product.component.css']
+  styleUrls: ['./add-product.component.css'],
 })
 export class AddProductComponent implements OnInit {
   //form controls
@@ -17,19 +17,20 @@ export class AddProductComponent implements OnInit {
   description!: FormControl;
   price!: FormControl;
   images!: FormControl;
-  user:any;
+  user: any;
   userImageUrl!: string;
 
-
-
-  constructor(private authService: AuthService, private productService: ProductService,private router: Router) {
+  constructor(
+    private authService: AuthService,
+    private productService: ProductService,
+    private router: Router
+  ) {
     this.initform();
     this.createForm();
   }
 
   ngOnInit(): void {
-    this.authService.getUser().subscribe((req)=>{
-
+    this.authService.getUser().subscribe((req) => {
       this.user = req.data.user;
       if (this.user.images.length > 0) {
         this.userImageUrl = this.user.images[0];
@@ -37,9 +38,15 @@ export class AddProductComponent implements OnInit {
     });
   }
   initform(): void {
-    this.name = new FormControl('', [Validators.required,regexValid(/[0-9]/g)]);
+    this.name = new FormControl('', [
+      Validators.required,
+      regexValid(/[0-9]/g),
+    ]);
     this.description = new FormControl('', [Validators.required]);
-    this.price = new FormControl('', [Validators.required,regexValid(/[A-Za-z]/g)]);
+    this.price = new FormControl('', [
+      Validators.required,
+      regexValid(/[A-Za-z]/g),
+    ]);
     this.images = new FormControl('', [Validators.required]);
   }
   createForm(): void {
@@ -60,7 +67,19 @@ export class AddProductComponent implements OnInit {
       formData.append('images', images.value, images.value.name);
     }
 
-    this.productService.addProduct('products/', formData).subscribe((response: any) => this.router.navigate(['/productSubmission']));
+    this.authService.getUser().subscribe((req) => {
+      if (req.data.user.role === 'admin') {
+        this.productService.addProduct('products/', formData).subscribe(() => {
+          this.router.navigate(['/products']);
+        });
+      } else {
+        this.productService
+          .addProduct('products/', formData)
+          .subscribe((response: any) =>
+            this.router.navigate(['/productSubmission'])
+          );
+      }
+    });
   }
 
   onImageSelected(event: Event) {
@@ -68,6 +87,4 @@ export class AddProductComponent implements OnInit {
     this.images.setValue(file);
     this.images.markAsTouched();
   }
-
 }
-
