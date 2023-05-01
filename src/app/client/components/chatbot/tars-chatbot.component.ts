e88ef8fed14056fdf21c7916f6e1b801d596f707
KhairouneAymen <aymen.khairoune@esprit.tn>
2023-05-01 14:54:48 +0100

    chatbot

---
+++ src/app/client/components/chatbot/tars-chatbot.component.ts
@@ -0,0 +1,16 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-chatbot',
+  templateUrl: './tars-chatbot.component.html',
+  styleUrls: ['./tars-chatbot.component.css']
+})
+export class TarsChatbotComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
