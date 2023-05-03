c210020bb4d25a40e7463652d23751f115d5e68f
aminhl <amin.hlel@esprit.tn>
2023-05-03 17:56:10 +0100

    ethereum_donation

--- src/app/client/components/donation/donation-details/donation-details.component.ts
+++ src/app/client/components/donation/donation-details/donation-details.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
-import { ProductService } from "../../../../shared/services/product.service";
 import { RequestService } from "../../../../shared/services/request.service";
+import { FormControl, NgForm } from "@angular/forms";
+import { DonationService } from "../../../../shared/services/donation.service";
+import { AuthService } from "../../../../shared/services/auth.service";
+import axios from "axios";
+import Swal from "sweetalert2";
+import { HttpErrorResponse } from "@angular/common/http";
 
 @Component({
   selector: 'app-donation-details',
@@ -11,7 +16,12 @@ import { RequestService } from "../../../../shared/services/request.service";
 export class DonationDetailsComponent implements OnInit {
   donation: any;
   donationImageUrl!: string;
-  constructor(private route: ActivatedRoute, private requestService: RequestService) { }
+  donationAmount: number = 0;
+  constructor(private route: ActivatedRoute,
+              private requestService: RequestService,
+              private donationService: DonationService,
+              private authService: AuthService) {
+  }
 
   ngOnInit(): void {
     const donationId = this.route.snapshot.paramMap.get('id');
@@ -21,6 +31,58 @@ export class DonationDetailsComponent implements OnInit {
         this.donationImageUrl = this.donation.requestImage[0];
       }
     });
+  }
+
+  // @ts-ignore
+  donateWithEthereum = async (requestId: string, toAddress?: string, usdAmount?: number) => {
+    const loggedUserData = this.authService.getConnectedUserData();
+    try {
+      const response = await axios
+        .get('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd');
+      const ethPrice = response.data.ethereum.usd;
+      const ethAmount = usdAmount / ethPrice;
+      return this.donationService.donateWithEthereum(requestId,{
+        fromAddress: loggedUserData.walletEth.address,
+        toAddress: toAddress,
+        privateKey: "0x70f34ac1d4d3e5fb97bc9326ed13533d975901130806664796196eeecbde036d",
+        amount: ethAmount.toFixed(8) // Specify 8 decimal places for ETH amount
+      }).subscribe((data) => {
+        const transactionHash = data.hash
+        setTimeout(() => {
+          Swal.fire({
+            icon: 'success',
+            title: 'Payment Successful',
+            text: 'Thank you for your payment!\n Transaction Hash: ' + transactionHash,
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'OK'
+          });
+        }, 2000)
+      },
+        (error: HttpErrorResponse) => {
+          if (error.status === 500) {
+            // Handle the error here
+            Swal.fire({
+              icon: 'error',
+              title: 'Payment Error',
+              text: 'No enough funds in your wallet',
+              confirmButtonColor: '#3085d6',
+              confirmButtonText: 'OK'
+            });
+          }
+        });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  onSubmit(form: NgForm) {
+    const requestBody = { amount: form.value.amount };
+    const requestId = this.route.snapshot.paramMap.get('id');
+    this.requestService.donateStripe(requestBody, requestId).subscribe(response => window.location.href = response.data.session_url);
+  }
+
+  onSliderChange(value: number) {
+    this.donationAmount = value ;
   }
 
 }
