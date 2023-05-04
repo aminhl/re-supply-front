01558da09963fa549af60776c2b1d149ca7a99ef
Hamza Ben Mahmoud <hamza.benmahmoud@esprit.tn>
2023-05-04 01:30:12 +0100

    Fixing error main c and IA Service

--- src/app/client/components/main-c/main-c.component.ts
+++ src/app/client/components/main-c/main-c.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from "../../../shared/services/auth.service";
-import { ProductService } from "../../../shared/services/product.service";
 
 @Component({
   selector: 'app-main-c',
@@ -10,22 +9,10 @@ import { ProductService } from "../../../shared/services/product.service";
 export class MainCComponent implements OnInit {
   verified: any;
   role: any;
-   products: any;
-   owner: any;
-  productImageUrl!: string;
 
-  constructor(private authService:AuthService,private productService:ProductService) { }
+  constructor(private authService:AuthService) { }
 
   ngOnInit(): void {
-    this.productService.getAcceptedProducts().subscribe((req)=>{
-      this.products = req.data.products;
-      this.owner = req.data.owner;
-
-      console.log(this.products)
-      if (this.products.images && this.products.images.length > 0) {
-        this.productImageUrl = this.products.images[0];
-      }
-    });
   }
 
   showUsers(){
