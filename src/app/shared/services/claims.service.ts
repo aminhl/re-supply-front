cfa182f63d521d2e291d3abcc167628f79418619
KhairouneAymen <aymen.khairoune@esprit.tn>
2023-05-09 18:16:49 +0100

    claims category

--- src/app/shared/services/claims.service.ts
+++ src/app/shared/services/claims.service.ts
@@ -2,26 +2,47 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import {environment as env} from "../../../environments/environment";
+import Sentiment from 'sentiment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClaimsService {
-
+  sentiment = new Sentiment();
   constructor(public http: HttpClient) { }
 
   addFeedback(target: string, title: string, message: string) {
+    const emotion = this.sentiment.analyze(message);
+    let category = '';
+
+    if (emotion.score > 0) {
+      category = 'positive';
+    } else if (emotion.score < 0) {
+      category = 'negative';
+    } else {
+      category = 'neutral';
+    }
+
     const requestBody = {
       title,
-      message
+      message,
+      category
     };
+
     return this.http.post(env.apiRoot + target, requestBody, {
       withCredentials: true,
     });
   }
 
+
   getAllFeedbacks(): Observable<any> {
     return this.http.get<any>(env.apiRoot + 'feedbacks', {  withCredentials: true,});
+  }
+  getPositiveFeedbacks(): Observable<any> {
+    return this.http.get<any>(env.apiRoot + 'feedbacks/positive', {  withCredentials: true,});
+  }
+  getNegativeFeedbacks(): Observable<any> {
+    return this.http.get<any>(env.apiRoot + 'feedbacks/negative', {  withCredentials: true,});
   }
 
 }
